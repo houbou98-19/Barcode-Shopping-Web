5375d5801ccdd7b8f9fe25a702cbcc60c38c93c1
houssam <houbou98@gmail.com>
2025-04-06 03:16:04 +0200

    KAN-6: Add barcode scanner, clean up assets, setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Scanner from './pages/Scanner.jsx'
 
-function App() {
-  return <h1>Hello, Shopping App Frontend!</h1>;
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/scanner" element={<Scanner />} />
+    </Routes>
+  )
 }
-
-export default App;
